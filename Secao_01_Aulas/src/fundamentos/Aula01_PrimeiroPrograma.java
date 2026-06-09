5881fc06f0c3f5ed4d3d2dd4821bbf6fde250ab0
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-06-08 22:47:10 -0300

    comentando dentro do codigo java

--- Secao_01_Aulas/src/fundamentos/Aula01_PrimeiroPrograma.java
+++ Secao_01_Aulas/src/fundamentos/Aula01_PrimeiroPrograma.java
@@ -2,8 +2,47 @@ package fundamentos;
 
 public class Aula01_PrimeiroPrograma {
 	
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		
+		//Comentando no código em uma linha: Uma sentença de código termina com ;
+		//Mais uma 
+
 		System.out.println("Olá programadores, meu nome é Francimilton! Este é meu primeiro Programa em java!");
+		System.out.println("Olá"); //comentário nas linhas de 
+
+		
+		/*
+		 * Comeentando com varias linhas
+		 
+		 * Linha 1
+		 * Linha 2
+		 
+		 
+		 */
+		
+		//javadoc:
+		
+		/**
+		 * JavaDoc é um padrão de documentação do Java.
+		 * Ele permite descrever classes, métodos e atributos
+		 * para facilitar o entendimento e a manutenção do código.
+		 *
+		 * A documentação pode ser gerada automaticamente
+		 * em páginas HTML usando a ferramenta javadoc.
+		 */
+		
+		//Exemplo abaixo:
+		
+		/**
+		 * @author Francimilton Sena <@email.com>
+		 * @since 1.0
+		 * 
+		 * @param args
+		 * 
+		 * Essa classe representa alguma informação relevante. Algo que o java vai conseguir interpretar 
+		 * para criar um javadoc
+		 */
+
 	}
 
 }
