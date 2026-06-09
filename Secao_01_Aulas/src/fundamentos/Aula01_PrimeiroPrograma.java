4c092d0edb4078380d55fbf9edf74fa00764adb6
francimiltondigitalhub <francimiltondigitalhub@gmail.com>
2026-06-08 22:34:45 -0300

    Inicio dos estudos em Java e organizacao de projetos

---
+++ Secao_01_Aulas/src/fundamentos/Aula01_PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class Aula01_PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá programadores, meu nome é Francimilton! Este é meu primeiro Programa em java!");
+	}
+
+}
---
+++ Secao_01_OrganizacaoDeProjetos/src/educacao/Prova.java
@@ -0,0 +1,5 @@
+package educacao;
+
+public class Prova {
+
+}
